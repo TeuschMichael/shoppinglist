0624a3ea27385227ef04e1d25367755c2ba5607f
TeuschMichael <127761578+TeuschMichael@users.noreply.github.com>
2023-12-15 13:56:31 +0100

    The sexton does not like eggs

--- backend/src/main/java/com/example/gitdemo/Controller.java
+++ backend/src/main/java/com/example/gitdemo/Controller.java
@@ -1,13 +1,15 @@
 package com.example.gitdemo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+// toet toet dit blokt github als het goed is
 @RestController
+@CrossOrigin("http://localhost:5173")
 @RequestMapping("/api/v1/shopping-list-items")
 public class Controller {
     @GetMapping
@@ -15,7 +17,7 @@ public class Controller {
         return List.of(
                 "boter",
                 "Kaas",
-                "Eieren"
+                "Spek"
         );
     }
 }
