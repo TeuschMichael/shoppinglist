e5b28a423b9514281a0aa8a7d15060118b63179f
TeuschMichael <127761578+TeuschMichael@users.noreply.github.com>
2023-12-15 12:17:49 +0100

    backend works

---
+++ backend/src/main/java/com/example/gitdemo/Controller.java
@@ -0,0 +1,21 @@
+package com.example.gitdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/shopping-list-items")
+public class Controller {
+    @GetMapping
+    public List<String> get() {
+        return List.of(
+                "boter",
+                "Kaas",
+                "Eieren"
+        );
+    }
+}
